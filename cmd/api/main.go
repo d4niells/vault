c6e4ea5071f1b3c226d39b3db623ab79d0dd7776
Daniel Oliveira <d4niells@duck.com>
2025-03-03 23:41:49 -0300

    feat(upload): publish message on RabbitMQ contaning 4KB file chunk

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,53 +1,90 @@
 package main
 
 import (
-  "net/http"
-  "io"
-  "os"
-  "bufio"
-  "log"
+	"bufio"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/rabbitmq/amqp091-go"
 )
 
+func publish(ch *amqp091.Channel, body []byte) error {
+	queue, err := ch.QueueDeclare("encrypt_chunks", true, false, false, false, nil)
+	if err != nil {
+		log.Printf("couldn't create encrypt_chunks queue: %s", err)
+		return err
+	}
+
+	err = ch.Publish("", queue.Name, false, false, amqp091.Publishing{
+		ContentType: "application/octet-stream",
+		Body:        body,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
-  r := http.NewServeMux()  
+	conn, err := amqp091.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Printf("couldn't open a new rabbitmq connection: %s", err)
+	}
+	defer conn.Close()
 
-  r.HandleFunc("POST /upload", func(w http.ResponseWriter, r *http.Request) {
-    f, fh, err := r.FormFile("file") 
-    if err != nil {
-      http.Error(w, "missing file on body request", http.StatusBadRequest)
-    }
-    defer f.Close()
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Printf("couldn't open a unique channel: %s", err)
+	}
+	defer ch.Close()
 
-    reader := bufio.NewReader(f)
-    buf := make([]byte, 1024 * 4) // 4KB chunks
-    for {
-      n, err := reader.Read(buf) 
-      if err != nil && err != io.EOF {
-        http.Error(w, "couldn't read the file", http.StatusInternalServerError)
-      }
-      if n == 0 {
-        break
-      }
+	r := http.NewServeMux()
 
-      chunk := buf[:n]
+	r.HandleFunc("POST /upload", func(w http.ResponseWriter, r *http.Request) {
+		f, _, err := r.FormFile("file")
+		if err != nil {
+			http.Error(w, "missing file on body request", http.StatusBadRequest)
+		}
+		defer f.Close()
 
-      log.Println("Name: ", fh.Filename)
-      log.Println("Size: ", fh.Size)
-      log.Println("Chunk: ", chunk)
-    }
-  })
+		reader := bufio.NewReader(f)
+		buf := make([]byte, 1024*4) // 4KB chunks
+		for {
+			n, err := reader.Read(buf)
+			if err != nil && err != io.EOF {
+				log.Printf("couldn't read the uploaded file with 4KB buffer: %s", err)
+				http.Error(w, "couldn't read the file", http.StatusInternalServerError)
+			}
+			if n == 0 {
+				break
+			}
 
-  port := os.Getenv("PORT")
-  if port == "" {
-    port = "8080"
-  }
+			chunk := buf[:n]
 
-  srv := http.Server{
-    Addr:     ":"+port,
-    Handler:  r,
-  }
+			err = publish(ch, chunk)
+			if err != nil {
+				log.Printf("couldn't publish the chunk's message: %s", err)
+				http.Error(w, "couldn't read the file", http.StatusInternalServerError)
+			}
+		}
 
-  if err := srv.ListenAndServe(); err != http.ErrServerClosed {
-    log.Fatalf("couldn't listen on port %s: %s", port, err)
-  }
+		w.WriteHeader(http.StatusCreated)
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	srv := http.Server{
+		Addr:    ":" + port,
+		Handler: r,
+	}
+
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Printf("couldn't listen on port %s: %s", port, err)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/d4niells/vault
 
 go 1.24.0
+
+require github.com/rabbitmq/amqp091-go v1.10.0 // indirect
