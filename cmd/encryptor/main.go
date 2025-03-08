be056513f31c47b3f908eb0feaf0708f35f99433
Daniel Oliveira <d4niells@duck.com>
2025-03-07 22:15:18 -0300

    feat(encryptor): encrypt the chunk file

--- cmd/encryptor/main.go
+++ cmd/encryptor/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 
@@ -12,6 +16,30 @@ import (
 type File struct {
 	Filename string
 	Chunk    []byte
+}
+
+func encrypt(chunk []byte) ([]byte, error) {
+	// TODO: create a new one and put it as an environment variable
+	key := []byte("thisisaverysecurekeyforAES123!")
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+	encryptedChunk := make([]byte, (1024*4)+gcm.NonceSize()) // 4KB + 12 bytes
+	encryptedChunk = gcm.Seal(nonce, nonce, chunk, nil)
+
+	return encryptedChunk, nil
 }
 
 func main() {
@@ -28,11 +56,17 @@ func main() {
 
 	var file File
 	for msg := range msgs {
-		// TODO: encrypt the hole chunk
 		err := json.Unmarshal(msg.Body, &file)
 		if err != nil {
 			log.Fatalf("couldn't unmarshal message: %s", err)
 		}
+
+		encryptedChunk, err := encrypt(file.Chunk)
+		if err != nil {
+			log.Fatalf("couldn't encrypt chunk: %s", err)
+		}
+
+		// TODO: publish the encrypted chunk into a encrypted_chunks queue
 
 		path := fmt.Sprintf("./tmp/%s", file.Filename)
 		f, err := os.OpenFile(path, os.O_WRONLY|os.O_APPEND, 0644)
@@ -43,7 +77,7 @@ func main() {
 			}
 		}
 
-		_, err = f.Write(file.Chunk)
+		_, err = f.Write(encryptedChunk)
 		if err != nil {
 			log.Fatalf("couldn't write file: %s", err)
 		}
